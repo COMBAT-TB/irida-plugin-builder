58189ff638966bf1205a51243280d86a91a32f37
Peter van Heusden <pvh@sanbi.ac.za>
2021-07-02 18:30:47 +0200

    Correct typo in insert_tool_file.sh

--- insert_tool_file.sh
+++ insert_tool_file.sh
@@ -8,7 +8,8 @@ fi
 ga_file=$1
 workflow_dir=$2
 version=$3
-jar_file=$4
+jar_filename=$4
 
 workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools_$version.yaml
 /usr/lib/jvm/default-jvm/bin/jar uf $jar_filename $workflow_dir/tools_$version.yaml
+rm $workflow_dir/tools_$version.yaml
