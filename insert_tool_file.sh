a7f2b0b52c879bb32d5c699314b59499d9e805b3
Peter van Heusden <pvh@sanbi.ac.za>
2021-07-02 19:05:10 +0200

    Remove version number in tools.yaml

--- insert_tool_file.sh
+++ insert_tool_file.sh
@@ -11,9 +11,9 @@ version=$3
 jar_filename=$4
 
 mkdir -p $workflow_dir
-workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools_$version.yaml
-/usr/lib/jvm/default-jvm/bin/jar uf $jar_filename $workflow_dir/tools_$version.yaml
-rm $workflow_dir/tools_$version.yaml
+workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools.yaml
+/usr/lib/jvm/default-jvm/bin/jar uf $jar_filename $workflow_dir/tools.yaml
+rm $workflow_dir/tools.yaml
 num_files=`ls $workflow_dir | wc -l`
 if [ $num_files -eq 0 ] ; then
   # remove empty workflow_dir
