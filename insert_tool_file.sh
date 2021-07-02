1b7da5b9eb61a520d1576d6ff6bd96d16afde6e6
Peter van Heusden <pvh@sanbi.ac.za>
2021-07-02 18:27:24 +0200

    Remove mkdir / rm from insert_tool_file.sh

--- insert_tool_file.sh
+++ insert_tool_file.sh
@@ -10,7 +10,5 @@ workflow_dir=$2
 version=$3
 jar_file=$4
 
-mkdir -p $workflow_dir
 workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools_$version.yaml
 /usr/lib/jvm/default-jvm/bin/jar uf $jar_filename $workflow_dir/tools_$version.yaml
-rm -rf $workflow_dir
