56c15421e9f3987a40fb0547ab48e789df7655a8
Peter van Heusden <pvh@sanbi.ac.za>
2021-07-02 16:02:28 +0200

    Try and run all Docker items as host user

--- build_workflow.sh
+++ build_workflow.sh
@@ -19,22 +19,24 @@ if [ ! -d $DESTINATION ] ; then
     exit 1
 fi
 
-docker run --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 mvn clean install
+docker run --user $(id -u) --group $(id -g) --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 mvn clean install
 jar_count=$(ls $WORKFLOW_NAME/target/*.jar |wc -l)
 if [ $jar_count -gt 1 ] ; then
   echo "WARNING: more than one output jar, some build steps will not run" >& 2
 else
   jar_filename=$(ls $WORKFLOW_NAME/target/*.jar)
 fi
-cp $WORKFLOW_NAME/target/*.jar $DESTINATION
-docker run --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 mvn clean
+
 for ga_file  in $(find $WORKFLOW_NAME -name \*.ga|sed 's^.*src/^src/^') ; do
   version=$(echo $ga_file|cut -d/ -f5)
   workflow_dir=$(dirname $ga_file)
   mkdir -p $workflow_dir
-  docker run --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools_$version.yaml
+  docker run --user $(id -u) --group $(id -g) --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 workflow-to-tools -w /pipelines/$ga_file -o $workflow_dir/tools_$version.yaml 
   if [ $jar_count -eq 1 ] ; then
     jar uf $jar_filename $workflow_dir/$tools_$version.yaml
   fi
   rm -rf $workflow_dir
 done
+
+cp $WORKFLOW_NAME/target/*.jar $DESTINATION
+docker run --user $(id -u) --group $(id -g) --workdir /pipelines/$WORKFLOW_NAME --rm -v $(pwd):/pipelines quay.io/combattb/irida-builder:21.05 mvn clean
