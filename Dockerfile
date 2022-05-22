abf0a80fb29339d9c003061e3aec697864cbc8f6
Peter van Heusden <pvh@sanbi.ac.za>
2022-05-22 17:01:52 +0200

    Merge changes tested on composer2

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL org.opencontainers.image.authors="Peter van Heusden <pvh@sanbi.ac.za>"
 ENV IRIDA_TAG=21.09.2
 RUN apt update && DEBIAN_FRONTEND="noninteractive" apt install -y default-jdk-headless maven git
 RUN git clone --depth 1 -b $IRIDA_TAG https://github.com/phac-nml/irida.git && cd irida/lib && bash install-libs.sh && cd ..
-RUN cd irida && mvn clean install -Djetty.skip=true -DskipTests
+RUN cd irida && sed -i 's/22.01-SNAPSHOT/21.09.2/' pom.xml && mvn clean install -Djetty.skip=true -DskipTests
 RUN cd /root/.m2/repository && apt install -y wget && wget https://github.com/phac-nml/irida-wf-ga2xml/releases/download/v1.1.0/irida-wf-ga2xml-1.1.0-standalone.jar 
 
 FROM alpine:latest 
--- insert_tool_file.sh
+++ insert_tool_file.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+set -e
+
 if [ $# != 4 ] ; then
   echo "Need 4 parameters, got: $*" >&2
   exit 1
@@ -11,8 +13,9 @@ version=$3
 jar_filename=$4
 
 mkdir -p $workflow_dir
-workflow_name = $(python -c 'import json; import sys; import shlex; data = json.load(open(sys.argv[1])); print(shlex.quote(data["name"]))' $ga_file) 
-workflow-to-tools -l $workflow_name -w /pipelines/$ga_file -o $workflow_dir/tools.yaml
+workflow_name=$(python3 -c 'import json; import sys; import shlex; data = json.load(open(sys.argv[1])); print(shlex.quote(data["name"]))' $ga_file) 
+
+workflow-to-tools -l Workflow_tools -w /pipelines/$ga_file -o $workflow_dir/tools.yaml
 /usr/lib/jvm/default-jvm/bin/jar uf $jar_filename $workflow_dir/tools.yaml
 rm $workflow_dir/tools.yaml
 num_files=`ls $workflow_dir | wc -l`
